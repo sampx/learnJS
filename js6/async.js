6223fc318097ed912c2ff8d0241060565973b65b
Samuel xu <samxu618@163.com>
2018-11-18 23:13:43 +0800

    remove superagent dependence

--- js6/async.js
+++ js6/async.js
@@ -4,7 +4,7 @@
   async function foo() {}
 
   // 函数表达式
-  const foo1 = async function () {};
+  const foo1 = async function() {};
 
   // 对象的方法
   let obj = { async foo() {} };
@@ -15,23 +15,24 @@
     constructor() {
       this.cachePromise = async () => {
         return '/avatars/sam.jpg';
-      }
+      };
     }
 
     async getAvatar(name) {
-      try{
+      try {
         const cache = await this.cachePromise();
         return cache.match(`/avatars/${name}.jpg`);
-      }catch(err){
+      } catch (err) {
         console.error(err);
         return 'no Avatar';
       }
-
     }
   }
 
   const storage = new Storage();
-  storage.getAvatar('sam').then((res)=>{console.log("avator of sam:",res)});
+  storage.getAvatar('sam').then(res => {
+    console.log('avator of sam:', res);
+  });
 
   // 箭头函数
   const foo2 = async () => {};
@@ -46,24 +47,24 @@
   // next
   const f1 = () => {
     //throw new Error('oh my god');
-    return new Promise((resolve) => {
-      setTimeout(resolve,20,'async now')
+    return new Promise(resolve => {
+      setTimeout(resolve, 20, 'async now');
     });
   };
   (async () => f1())() //f1是异步的，就可以用then指定下一步 见:promise.js Promise.try
-    .then((ret) => console.log(ret))
-    .catch((err) => console.error(err)); //注意:async f1()抛出的错误。所以，要使用catch方法。
+    .then(ret => console.log(ret))
+    .catch(err => console.error(err)); //注意:async f1()抛出的错误。所以，要使用catch方法。
   console.log('async next');
 }
 
 {
   //return 语句会成为then回调函数的参数
-  async function f() {    
+  async function f() {
     return 'hello world';
     throw new Error('出错了');
   }
 
-  f().then(v => console.log(v),e => console.log(e.message))
+  f().then(v => console.log(v), e => console.log(e.message));
 }
 
 {
@@ -74,16 +75,17 @@
     return html.match(/<title>([\s\S]+)<\/title>/i)[1];
   }
   //只有这三个操作全部完成，才会执行then方法里面的console.log,除非遇到return
-  getTitle('https://tc39.github.io/ecma262/').then(console.log,e => console.log(e.message))
+  getTitle('https://tc39.github.io/ecma262/').then(console.log, e =>
+    console.log(e.message)
+  );
 }
-
 
 {
   async function f() {
     return await 123; //转成 Promise 对象，并立即resolve
   }
 
-  f().then(v => console.log(v))
+  f().then(v => console.log(v));
   // 123
 }
 
@@ -93,29 +95,28 @@
     await Promise.resolve('hello world'); // 不会执行
   }
 
-  f().then(v => console.log(v),e => console.log(e));
+  f().then(v => console.log(v), e => console.log(e));
   // 出错了
 }
 
 {
   async function f() {
     try {
-      await new Promise(function (resolve, reject) {
+      await new Promise(function(resolve, reject) {
         throw new Error('出错了');
       });
-    } catch(e) {
-    }
-    return await('hello world');
+    } catch (e) {}
+    return await 'hello world';
   }
 
   f()
     .then(v => console.log(v))
-    .catch(e => console.log(e))
+    .catch(e => console.log(e));
   // 出错了
 }
 
 {
-  const superagent = require('superagent');
+  //const superagent = require('superagent');
   const NUM_RETRIES = 3;
 
   async function test() {
@@ -124,7 +125,7 @@
       try {
         //await superagent.get('http://google.com/this-throws-an-error');
         break;
-      } catch(err) {}
+      } catch (err) {}
     }
     console.log(i); // 3
   }
@@ -134,27 +135,26 @@
 
 {
   async function dbExec(db) {
-    let docs = [{1:1}, {2:2}, {3:3}];
-    let promises = docs.map((doc) => db.post(doc));
+    let docs = [{ 1: 1 }, { 2: 2 }, { 3: 3 }];
+    let promises = docs.map(doc => db.post(doc));
 
     let results = await Promise.all(promises);
     console.log(results);
   }
 
   let database = {
-    post : (param) => {
-      console.log('DB exec sql with params:'+JSON.stringify(param));
-      return new Promise((resolve) => resolve(param));
-    },
+    post: param => {
+      console.log('DB exec sql with params:' + JSON.stringify(param));
+      return new Promise(resolve => resolve(param));
+    }
   };
 
   // dbExec(database);
-
 }
 
 {
   async function dbExec(db) {
-    let docs = [{1:1}, {2:2}, {3:3}];
+    let docs = [{ 1: 1 }, { 2: 2 }, { 3: 3 }];
     // let promises = docs.map((doc) => db.post(doc));
 
     const promises = docs.map(async doc => {
@@ -170,7 +170,8 @@
 
     //results = promises.map(async (promise) => {return (await promise)}); //无法执行promise
 
-    for (let promise of promises) {//ok
+    for (let promise of promises) {
+      //ok
       results.push(await promise);
     }
 
@@ -178,14 +179,13 @@
   }
 
   let database = {
-    post : async (param) => {
-      console.log('DB exec sql with params:'+JSON.stringify(param));
+    post: async param => {
+      console.log('DB exec sql with params:' + JSON.stringify(param));
       return await param;
-    },
+    }
   };
 
   dbExec(database);
-
 }
 
 {
@@ -195,7 +195,6 @@
   //     console.log(x);
   //   }
   // })();
-
   //异步生成器函数
   // async function* asyncGenerator() {
   //   console.log('Start');
@@ -208,4 +207,3 @@
   //   .next()
   //   .catch(err => console.log(err)); // Error: Problem!
 }
-
